8ff90d2a0f686b8d2a99657fc9bb47d9d0c296e1
Jake Pember <jake.pember@kuleuven.be>
2024-10-24 10:23:31 -0700

    More work on drift calculation

--- mask_deltas.py
+++ mask_deltas.py
@@ -1,6 +1,13 @@
 """
-A script to calculate differences between previously computed lists of etalon
-line wavelengths.
+Compute (chromatic) etalon drift from a catalogue of Polly etalon masks.
+
+For each individual etalon peak identified in a reference mask, trace the peak
+location over time. Then, fit a linear trend to the difference between these
+wavelengths and the original reference wavelength. The slope of this trend line
+describes the speed of wavelength drift over time of the individual line.
+Plot these slopes as a function of wavelength in order to visualise the
+chromatic drift of the etalon
+
 """
 
 
@@ -55,9 +62,18 @@ class PeakDrift:
 
 
     def __post_init__(self):
+        
+        print(f"Tracking drift for λ={self.reference_wavelength:.1f}...")
+        
         self.track_drift()
+        self.linear_fit()
         
-        
+    
+    @property
+    def valid_wavelengths(self) -> list[float]:
+        return list(np.array(self.wavelengths)[self.valid])
+    
+    
     @property
     def reference_date(self) -> datetime:
         return parse_filename(self.reference_mask).date
@@ -150,27 +166,26 @@ class PeakDrift:
           day?
         """
         
-        if not self.wavelengths:
+        if not self.valid_wavelengths:
             print("No valid wavelengths found. Run PeakDrift.track_drift() first.")
             return
         
         ref_wl = self.reference_wavelength * u.Angstrom
-        wls = self.wavelengths * u.Angstrom
-        deltas = (wls - ref_wl)
+        wls = self.valid_wavelengths * u.Angstrom
+        deltas = (wls - ref_wl).to(u.Angstrom).value
         
         d0 = self.reference_date
         days = [(d - d0).days for d in self.dates]
         
         p, cov = np.polyfit(x = days, y = deltas, deg = 1, cov = True)
         
-        print(f"{ref_wl = }")
-        print(f"{p = }")
-        print(f"{cov = }")
-        
         self.fit = np.poly1d(p)
         self.fit_err = np.sqrt(np.diag(cov))
-        self.fit_slope = p[0]
-        self.fit_slope_err = self.fit_err[0]
+        self.fit_slope = p[0] * u.Angstrom / u.day
+        self.fit_slope_err = self.fit_err[0] * u.Angstrom / u.day
+        
+        # print(f"{self.reference_wavelength:.3e = }")
+        # print(f"{self.fit_slope:.3e = } +/- {self.fit_slope_err:.3e}")
         
         return
         
@@ -247,130 +262,6 @@ def savitzky_golay(
     lastvals  = y[-1] + np.abs(y[-half_window-1: -1][::-1] - y[-1])
     y = np.concatenate((firstvals, y, lastvals))
     return np.convolve(m[::-1], y, mode='valid')
-
-
-def deltas_vector(
-    reference_mask: str,
-    mask: str,
-    intermediate_mask: str = None,
-    ) -> list[tuple[float]]:
-    
-    with open(reference_mask, "r") as f:
-        reference_peaks =\
-            np.array(
-                [float(line.strip().split()[0]) for line in f.readlines()[1:]]
-                )
-            
-    peak_spacing = np.diff(reference_peaks)
-
-    deltas: list[float] = []
-    
-    print(f"{mask} ", end="")
-    with open(mask, "r") as f:
-        peaks =\
-            [float(line.strip().split()[0]) for line in f.readlines()[1:]]
-
-    for i, peak in enumerate(peaks):
-        try:
-            local_spacing = peak_spacing[i]
-        except IndexError:
-            try:
-                local_spacing = peak_spacing[i-1]
-            except IndexError:
-                local_spacing = 0
-            
-        distances = np.abs(reference_peaks - peak)
-        
-        try:
-            # Find the closest reference peak in wavelength
-            closest_index = np.nanargmin(distances)
-        except ValueError as e:
-            # print(e)
-            # Print a dot for any missing peaks
-            print(".", end="")
-            closest_index = -1
-        
-        reference_peak = reference_peaks[closest_index]    
-        delta = reference_peak - peak
-
-        # Check that we are in fact looking at the same peak!
-        if abs(delta) <= local_spacing / 100:
-            deltas.append((reference_peak, delta))
-        else:
-            # Print a comma for any peak out of range
-            print(",", end="")
-            deltas.append((reference_peak, None)) 
-    print()
-            
-    return deltas
-
-
-def compute_deltas(
-    reference_mask: str,
-    masks: list[str],
-    ) -> dict[str, list[tuple[float]]]:
-
-    deltas: dict[str, list[float]] =\
-        {mask: deltas_vector(reference_mask, mask) for mask in masks}
-            
-    return deltas
-    
-    
-def plot_deltas(
-    deltas: dict[str, list[tuple[float]]],
-    reference_mask: str = None, # File path
-    ax: plt.axes = None,
-    smoothed: bool = True,
-    xlim: tuple[float] = (440, 880),
-    ylim: tuple[float] = (-1, 1),
-    ) -> None:
-    
-    for mask, _deltas in deltas.items():
-        
-        try:
-            wavelengths = np.transpose(_deltas)[0] * u.angstrom
-            offsets = np.transpose(_deltas)[1] * u.angstrom
-        except Exception as e:
-            continue
-        
-        if ax is None:
-            fig = plt.figure(figsize = (12, 8))
-            ax = fig.gca()
-            ax.set_xlim(xlim)
-            ax.set_ylim(ylim)
-        
-        if smoothed:
-            
-            smooth_y = savitzky_golay(
-                y = offsets.to(u.pm).value,
-                window_size = 51,
-                order = 5,
-                )
-            
-            ax.plot(
-                wavelengths.to(u.nm).value[~np.isnan(smooth_y)],
-                smooth_y[~np.isnan(smooth_y)],
-                alpha=0.5
-                )
-        
-        else:
-            ax.scatter(
-                wavelengths.to(u.nm).value,
-                offsets.to(u.pm).value,
-                marker=".", alpha=0.2
-                )
-                
-    if reference_mask:
-        ax.plot(0, 0, lw=0, label=f"Reference: {mask.split('/')[-1]}")
-        
-    ax.legend()
-
-        
-    # ax.set_title(f"{mask.split('/')[-1]}", size=20)
-    ax.set_xlabel("Wavelength [nm]", size=16)
-    ax.set_ylabel("Wavelength offset from\nreference mask [pm]", size=16)
-
-    plt.savefig(f"{OUTPUT_DIR}/_temp.png")
     
     
 def parse_date_string(datestr: str) -> datetime:
@@ -400,14 +291,22 @@ def parse_filename(filename: str | list[str]) -> tuple[datetime, str, str]:
     
     
 def find_mask(
-    date: str,
     masks: list[str],
+    datestr: str | None = None,
+    date: datetime | None = None,
     timeofday: str = "eve",
     orderlet: str = "SCI2",
     ) -> str:
     
-    date = parse_date_string(date)
-    
+    if (datestr is None and date is None) or (datestr and date):
+        print("Either datestr or date must be specified, not both")
+        
+    if date:
+        assert isinstance(date, datetime)
+        
+    if datestr:
+        date = parse_date_string(datestr)
+        
     for m in masks:
         mdate, mtimeofday, morderlet = parse_filename(m)
         if mdate == date and mtimeofday == timeofday and morderlet == orderlet:
@@ -454,21 +353,22 @@ def main() -> None:
     MASKS_DIR  = "/scr/jpember/polly_outputs/masks"
     OUTPUT_DIR = "/scr/jpember/temp"
     
+    ref_date = datetime(2024, 5, 1)
+    max_date = datetime(2024, 7, 1)
+    
     masks = sorted(glob(f"{MASKS_DIR}/*SCI2*.csv"))
+    
     
     masks = select_masks(
         masks = masks,
-        min_date = datetime(2024, 3, 1),
+        min_date = ref_date,
+        max_date = max_date,
         orderlet = "SCI2",
         timeofday = "eve",
         )
     
-    # from pprint import pprint
-    # pprint(masks)
+    reference_mask = find_mask(date=ref_date, masks=masks)
     
-    reference_mask = find_mask(date="20240301", masks=masks)
-    
-
     with open(reference_mask, "r") as f:
         reference_wavelengths = np.array(
             [float(line.strip().split()[0]) for line in f.readlines()[1:]]
@@ -484,22 +384,55 @@ def main() -> None:
             masks = masks,
             )
                 for reference_wavelength, local_spacing
-                in zip(reference_wavelengths[::], local_spacings[::])
+                in zip(
+                    reference_wavelengths[::250],
+                    local_spacings[::250]
+                    )
         ]
-
-    fig = plt.figure(figsize=(12, 10))
-    ax = fig.gca()
-        
-    for d in drifts[::]:
-        ax.plot(d.dates, 1_000 * d.deltas, alpha=0.25)
-        
-    ax.set_ylim(-0.5, 1)
-    ax.set_ylabel("Drift from reference wavelength [pm]")
     
+    
+    # Calculate and save slopes to file
+    # wavelengths = [d.reference_wavelength for d in drifts]
+    # slopes = [d.fit_slope.to(u.Angstrom / u.day).value for d in drifts] * u.Angstrom / u.day
+    
+    # np.savetxt(
+    #     f"{OUTPUT_DIR}/drifts_{ref_date:%Y%m%d}_{max_date:%Y%m%d}",
+    #     np.transpose([wavelengths, slopes.to(u.Angstrom / u.day).value])
+    #     )
+
+
+
+    # Plot drift over time
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.gca()
+    
+    for d in drifts:
+        if d.reference_wavelength > 5000:
+            if 6000 <= d.reference_wavelength <= 6100:
+                continue
+            ax.plot(d.dates, d.deltas, alpha=0.5)
+        
+    ax.set_ylim(-0.0002, 0.0005)
+    ax.set_ylabel("Drift from reference wavelength [Angstroms]")
     ax.set_xlabel("Time")
         
-    plt.savefig(f"{OUTPUT_DIR}/_temp.png")
+    plt.savefig(f"{OUTPUT_DIR}/deltas_{ref_date:%Y%m%d}_{max_date:%Y%m%d}.png")
     plt.close()
+
+
+
+    # Plot slopes as a function of wavelength
+    # fig = plt.figure(figsize=(10, 8))
+    # ax = fig.gca()
+    
+    # ax.scatter(wavelengths, slopes, s=10, alpha=0.25)
+        
+    # ax.set_ylim(-5e-6, 1e-5)
+    # ax.set_ylabel("Daily drift in peak location [Angstroms per day]")
+    # ax.set_xlabel("Wavelength [Angstroms]")
+        
+    # plt.savefig(f"{OUTPUT_DIR}/_temp.png")
+    # plt.close()
 
 
 if __name__ == "__main__":
