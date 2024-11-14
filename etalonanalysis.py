c3f2058b3410e65dba38bdf4ae1ca3343f10cb1d
Jake Pember <jake.pember@kuleuven.be>
2024-11-14 03:28:34 -0800

    Remove old printing colour codes

--- etalonanalysis.py
+++ etalonanalysis.py
@@ -87,14 +87,6 @@ except ImportError:
 plt.style.use(plotStyle)
 
 
-HEADER  = '\033[95m'
-OKBLUE  = '\033[94m'
-OKGREEN = '\033[92m'
-WARNING = '\033[93m'
-FAIL    = '\033[91m'
-ENDC    = '\033[0m'
-
-
 @dataclass
 class Peak:
     """
