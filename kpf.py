a4f4ede27ec18d63260d7f270d10f7fead56d092
Jake Pember <jake.pember@kuleuven.be>
2024-11-15 04:19:31 -0800

    Move ORDERLETS and TIMESOFDAY to kpf module

--- kpf.py
+++ kpf.py
@@ -4,3 +4,7 @@ Parameters specific to the KPF spectrograph
 
 
 THORIUM_ORDER_INDICES = [*list(range(12)), 35]
+
+ORDERLETS = ["SCI1", "SCI2", "SCI3", "CAL", "SKY"]
+
+TIMESOFDAY = ["morn", "day", "eve", "night", "midnight"]
