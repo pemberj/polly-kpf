8adc610831fa6897a0e5b706fe50010359237a73
Jake Pember <jake.pember@kuleuven.be>
2024-12-06 01:49:37 -0800

    Add LFC_ORDER_INDICES list

--- kpf.py
+++ kpf.py
@@ -5,6 +5,8 @@ Parameters specific to the KPF spectrograph
 
 THORIUM_ORDER_INDICES = [*list(range(12)), 35]
 
+LFC_ORDER_INDICES = [i for i in range(67) if i not in THORIUM_ORDER_INDICES]
+
 ORDERLETS = ["SCI1", "SCI2", "SCI3", "CAL", "SKY"]
 
 TIMESOFDAY = ["morn", "day", "eve", "night", "midnight"]
